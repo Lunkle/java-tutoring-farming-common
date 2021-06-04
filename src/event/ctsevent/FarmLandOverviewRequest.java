bf0ebdd730f18795a563efb39d9fa441e8bed313
Lunkle <bobbob.donny@gmail.com>
2021-06-04 19:43:50 -0400

    Formatting

--- src/event/ctsevent/FarmLandOverviewRequest.java
+++ src/event/ctsevent/FarmLandOverviewRequest.java
@@ -8,27 +8,27 @@ public class FarmLandOverviewRequest extends CTSEvent implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = -5736056546837007817L;
-	private int coordinateX;
 
-	private int coordinateY;
+	private int x;
+	private int y;
 
-	public FarmLandOverviewRequest(long id, int coordinateX, int coordinateY) {
+	public FarmLandOverviewRequest(long id, int x, int y) {
 		super(id);
-		this.coordinateX = coordinateX;
-		this.coordinateY = coordinateY;
+		this.x = x;
+		this.y = y;
 	}
 
-	public int getCoordinateX() {
-		return coordinateX;
+	public int getX() {
+		return x;
 	}
 
-	public int getCoordinateY() {
-		return coordinateY;
+	public int getY() {
+		return y;
 	}
 
 	@Override
 	public String getDescription() {
-		return "Fetching farmland overview (" + coordinateX + ", " + coordinateY + ")";
+		return "Fetching farmland overview (" + x + ", " + y + ")";
 	}
 
 }
