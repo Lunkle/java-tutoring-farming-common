24b6a7fc9442b444a22e0fb763bdc4b0b605216c
JaryJay <jay.jrjren@gmail.com>
2021-06-05 11:16:21 -0400

    Removed plant name from SowSeedFailResponse and SowSeedSuccessResponse

--- src/event/stcevent/game/SowSeedFailResponse.java
+++ src/event/stcevent/game/SowSeedFailResponse.java
@@ -5,18 +5,16 @@ import event.stcevent.STCEvent;
 public class SowSeedFailResponse extends STCEvent {
 
 	private static final long serialVersionUID = -805833691612948920L;
-	private String plantName;
 	private String failReason;
 
-	public SowSeedFailResponse(long id, long respondingTo, String plantName, String failReason) {
+	public SowSeedFailResponse(long id, long respondingTo, String failReason) {
 		super(id, respondingTo);
-		this.plantName = plantName;
 		this.failReason = failReason;
 	}
 
 	@Override
 	public String getDescription() {
-		return "Failed to sow a " + plantName + " seed. " + failReason;
+		return "Failed to sow seed. " + failReason;
 	}
 
 }
--- src/event/stcevent/game/SowSeedSuccessResponse.java
+++ src/event/stcevent/game/SowSeedSuccessResponse.java
@@ -5,16 +5,14 @@ import event.stcevent.STCEvent;
 public class SowSeedSuccessResponse extends STCEvent {
 
 	private static final long serialVersionUID = -805833691612948920L;
-	private String plantName;
 
-	public SowSeedSuccessResponse(long id, long respondingTo, String plantName) {
+	public SowSeedSuccessResponse(long id, long respondingTo) {
 		super(id, respondingTo);
-		this.plantName = plantName;
 	}
 
 	@Override
 	public String getDescription() {
-		return "Successfully sowed a " + plantName + " seed.";
+		return "Successfully sowed seed.";
 	}
 
 }
