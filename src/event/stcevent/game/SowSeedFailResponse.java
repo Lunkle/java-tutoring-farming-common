88e3fbb3eedad8544f7b353fabd4d0aaa82e17f4
JaryJay <jay.jrjren@gmail.com>
2021-06-05 11:13:46 -0400

    Added "Response" to the end of SowSeedFail and SowSeedSuccess

--- src/event/stcevent/game/SowSeedFailResponse.java
+++ src/event/stcevent/game/SowSeedFailResponse.java
@@ -2,13 +2,13 @@ package event.stcevent.game;
 
 import event.stcevent.STCEvent;
 
-public class SowSeedFail extends STCEvent {
+public class SowSeedFailResponse extends STCEvent {
 
 	private static final long serialVersionUID = -805833691612948920L;
 	private String plantName;
 	private String failReason;
 
-	public SowSeedFail(long id, long respondingTo, String plantName, String failReason) {
+	public SowSeedFailResponse(long id, long respondingTo, String plantName, String failReason) {
 		super(id, respondingTo);
 		this.plantName = plantName;
 		this.failReason = failReason;
--- src/event/stcevent/game/SowSeedSuccessResponse.java
+++ src/event/stcevent/game/SowSeedSuccessResponse.java
@@ -2,12 +2,12 @@ package event.stcevent.game;
 
 import event.stcevent.STCEvent;
 
-public class SowSeedSuccess extends STCEvent {
+public class SowSeedSuccessResponse extends STCEvent {
 
 	private static final long serialVersionUID = -805833691612948920L;
 	private String plantName;
 
-	public SowSeedSuccess(long id, long respondingTo, String plantName) {
+	public SowSeedSuccessResponse(long id, long respondingTo, String plantName) {
 		super(id, respondingTo);
 		this.plantName = plantName;
 	}
