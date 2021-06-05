db5825908154475a4d8f5af8031e4ff9def143e4
Lunkle <bobbob.donny@gmail.com>
2021-06-05 10:06:29 -0400

    Formatting

--- src/event/stcevent/game/FarmLandOverviewResponse.java
+++ src/event/stcevent/game/FarmLandOverviewResponse.java
@@ -26,7 +26,7 @@ public class FarmLandOverviewResponse extends STCEvent implements Serializable {
 
 	@Override
 	public String getDescription() {
-		String string = "==FarmLand==\n";
+		String string = "\n==FarmLand==\n";
 		for (int row = 0; row < 4; row++) {
 			for (int col = 0; col < 4; col++) {
 				string += '[';
