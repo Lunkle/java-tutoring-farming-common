2d48840c4ccebb1428b9045d52c46984e374e78b
Lunkle <bobbob.donny@gmail.com>
2021-06-05 14:14:24 -0400

    Update FarmLandNotOwnedResponse.java

--- src/event/stcevent/game/FarmLandNotOwnedResponse.java
+++ src/event/stcevent/game/FarmLandNotOwnedResponse.java
@@ -1,1 +1,20 @@
+package event.stcevent.game;
 
+import java.io.Serializable;
+
+import event.stcevent.STCEvent;
+
+public class FarmLandNotOwnedResponse extends STCEvent implements Serializable {
+
+	private static final long serialVersionUID = -4770847606385112672L;
+
+	public FarmLandNotOwnedResponse(long id, long respondingTo) {
+		super(id, respondingTo);
+	}
+
+	@Override
+	public String getDescription() {
+		return "You don't own this farm land";
+	}
+
+}
