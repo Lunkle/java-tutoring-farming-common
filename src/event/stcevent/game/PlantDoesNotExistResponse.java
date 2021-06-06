76ffcdd93c6a10f5fa8f64bc0f995816f8ce3d18
JaryJay <jay.jrjren@gmail.com>
2021-06-06 12:26:59 -0400

    Update PlantDoesNotExistResponse.java

---
+++ src/event/stcevent/game/PlantDoesNotExistResponse.java
@@ -0,0 +1,20 @@
+package event.stcevent.game;
+
+import java.io.Serializable;
+
+import event.stcevent.STCEvent;
+
+public class PlantDoesNotExistResponse extends STCEvent implements Serializable {
+
+	private static final long serialVersionUID = -4454796821747924108L;
+
+	public PlantDoesNotExistResponse(long id, long respondingTo) {
+		super(id, respondingTo);
+	}
+
+	@Override
+	public String getDescription() {
+		return "Plant does not exist.";
+	}
+
+}
