db20fe94253ff2adf1e929361f75275ab6e14246
Lunkle <bobbob.donny@gmail.com>
2021-06-05 20:40:39 -0400

    Update SaveRequest.java

--- src/event/ctsevent/session/SaveRequest.java
+++ src/event/ctsevent/session/SaveRequest.java
@@ -7,26 +7,14 @@ import event.ctsevent.CTSEvent;
 public class SaveRequest extends CTSEvent implements Serializable {
 
 	private static final long serialVersionUID = -876043311513972309L;
-	private String username;
-	private String password;
 
-	public SaveRequest(long id, String username, String password) {
+	public SaveRequest(long id) {
 		super(id);
-		this.username = username;
-		this.password = password;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public String getPassword() {
-		return password;
 	}
 
 	@Override
 	public String getDescription() {
-		return "Attempting login";
+		return "Saving";
 	}
 
 }
