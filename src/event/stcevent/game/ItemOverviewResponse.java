bfa4ca41b7d67260a855a5b0aa54fb22fcbf96af
Lunkle <bobbob.donny@gmail.com>
2021-06-13 23:08:00 -0400

    Update ItemOverviewResponse.java

--- src/event/stcevent/game/ItemOverviewResponse.java
+++ src/event/stcevent/game/ItemOverviewResponse.java
@@ -4,16 +4,19 @@ import event.stcevent.STCEvent;
 
 public class ItemOverviewResponse extends STCEvent {
 
-	private static final long serialVersionUID = 8551497087266206540L;
+	private static final int MS_PER_MINUTE = 60000;
+	private static final int MS_PER_HOUR = 3600000;
 	private String description;
 	private int value;
 	private String growsInto;
 	private int nutritionValue;
+	private long sellsIn;
 
-	public ItemOverviewResponse(long id, long respondingTo, String description, int value, String growsInto, int nutritionValue) {
+	public ItemOverviewResponse(long id, long respondingTo, String description, int value, long sellsIn, String growsInto, int nutritionValue) {
 		super(id, respondingTo);
 		this.description = description;
 		this.value = value;
+		this.sellsIn = sellsIn;
 		this.growsInto = growsInto;
 		this.nutritionValue = nutritionValue;
 	}
@@ -22,13 +25,20 @@ public class ItemOverviewResponse extends STCEvent {
 	protected String doGetDescription() {
 		String string = "\n===Item Description============"
 				+ '\n' + description
-				+ "\nValue: " + value + " gold coins";
+				+ "\nValue: " + value + " gold coins"
+				+ "\nSells in: " + formatDuration(sellsIn);
 		if (growsInto != null) {
 			string += "\nGrows: " + growsInto
 					+ "\nNutrition: " + nutritionValue;
 		}
 		string += "\n===============================";
 		return string;
+	}
+
+	private String formatDuration(long millis) {
+		return millis / MS_PER_HOUR + "h "
+				+ (millis % MS_PER_HOUR) / MS_PER_MINUTE + "m "
+				+ (millis % MS_PER_MINUTE) / 1000 + "s";
 	}
 
 }
