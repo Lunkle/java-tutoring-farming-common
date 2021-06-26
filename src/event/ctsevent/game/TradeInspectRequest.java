fa9644e8e0cf22749bbaa9e135dc25966d4b4919
Misha Melnyk <as.k.github@melnyk.org>
2021-06-26 19:49:30 -0400

    Added Trade inspection requests

---
+++ src/event/ctsevent/game/TradeInspectRequest.java
@@ -0,0 +1,30 @@
+package event.ctsevent.game;
+
+import event.ctsevent.CTSEvent;
+
+public class TradeInspectRequest extends CTSEvent {
+
+	private static final long serialVersionUID = -601352504355792134L;
+	private int row;
+	private int col;
+
+	public TradeInspectRequest(long id, int row, int col) {
+		super(id);
+		this.row = row;
+		this.col = col;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public int getCol() {
+		return col;
+	}
+
+	@Override
+	protected String doGetDescription() {
+		return "Inspecting trade posting at slot (" + row + ", " + col + ")";
+	}
+
+}
